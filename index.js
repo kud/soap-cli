324692d555219863863383f082ccb7ec22a1473b
Erwann Mest <emest@theorchard.com>
2022-12-31 01:39:54 +0100

    💄 Better UI

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
-import { $ } from "zx"
-$.verbose = false // enable it to debug
-
 import inquirer from "inquirer"
 import signale from "signale"
 import { deleteAsync } from "del"
+import chalk from "chalk"
 
 import {
   appNameFromPath,
@@ -14,52 +12,66 @@ import {
 
 const [appPath] = process.argv.slice(2)
 
-console.log("Welcome to soap 🧼, the app cleaner.")
-
-console.log("")
-
-const appName = appNameFromPath(appPath)
-const bundleId = await getBundleIdentifier(appName)
-const appFiles = await findAppFiles(appName, bundleId)
-
-signale.info(
-  `You want me to clean the application "${appName}". This is what I've found.`,
+console.log(
+  `Welcome to ${chalk.bold("soap")} 🧼, ${chalk.italic("the app cleaner")}.\n`,
 )
 
-console.log("")
+try {
+  const appName = appNameFromPath(appPath)
+  const bundleId = await getBundleIdentifier(appName)
+  const appFiles = await findAppFiles(appName, bundleId)
 
-const { deletedFilesWish, isConfirmed } = await inquirer.prompt([
-  {
-    type: "checkbox",
-    name: "deletedFilesWish",
-    message: "Select the files you want to delete",
-    choices: appFiles.map((appFile) => ({
-      name: appFile,
-      value: appFile,
-      checked: true,
-    })),
-  },
-  {
-    type: "confirm",
-    name: "isConfirmed",
-    message: "Are you sure?",
-    default: false,
-  },
-])
+  signale.info(
+    `You want me to clean this application: ${chalk.bold(appName)} 📦.`,
+  )
 
-console.log("")
+  signale.info(
+    `I also assume you gave me an application path. ${chalk.bold(
+      "No homebrew cask will be deleted then",
+    )}.`,
+  )
 
-if (!isConfirmed) {
-  signale.info(`Okay, mission aborded.`)
+  console.log("")
 
-  process.exit()
+  const { deletedFilesWish, isConfirmed } = await inquirer.prompt([
+    {
+      type: "checkbox",
+      name: "deletedFilesWish",
+      message:
+        "This is what I've found about it. Please select the files you want to delete.",
+      choices: appFiles.map((appFile) => ({
+        name: appFile,
+        value: appFile,
+        checked: true,
+      })),
+    },
+    {
+      type: "confirm",
+      name: "isConfirmed",
+      message: "Are you sure?",
+      default: false,
+    },
+  ])
+
+  if (!isConfirmed) {
+    console.log("")
+
+    signale.info(`Okay, mission aborded.`)
+
+    process.exit()
+  }
+
+  const deletedPaths = await deleteAsync(deletedFilesWish, { force: true })
+
+  console.log("")
+
+  console.log("Files deleted:")
+  deletedPaths.forEach((deletedPath) => console.log(`· ${deletedPath}`))
+
+  console.log("")
+
+  signale.success("Operation successful.")
+} catch (error) {
+  signale.error(`Something wrong appeared. Check the log below.\n`)
+  console.error(error)
 }
-
-const deletedPaths = await deleteAsync(deletedFilesWish, { force: true })
-
-console.log("Files deleted:")
-deletedPaths.forEach((deletedPath) => console.log(`· ${deletedPath}`))
-
-console.log("")
-
-signale.success("Operation successful.")
